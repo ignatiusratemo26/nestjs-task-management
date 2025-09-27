25a6809adc5a3f484e297dafdc99969abb2bfd98
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2025-09-27 15:13:59 +0300

    created a task model and task creation endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
 
     @Get()
-    async getAllTasks() {
-        return this.tasksService.getAllTasks();
+    async getAllTasks(): Promise<Task[]> {
+        return await this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    async createTask(@Body() body : { title: string, description: string}): Promise<Task> {
+        return await this.tasksService.createTask(body.title, body.description);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class TasksService {
-    private tasks = [4, 5, 6];
+    private tasks: Task[] = [];
 
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    async createTask(title: string, description: string): Promise<Task> {
+        const { v4: uuidv4 } = await import('uuid');
+        const task: Task = {
+            id: uuidv4(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;  
     }
 
 }
