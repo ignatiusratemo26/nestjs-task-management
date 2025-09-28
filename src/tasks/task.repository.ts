c67070a7312956e58078cad00598a104fc816e40
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2025-09-28 12:49:12 +0300

    Add a task repository

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,9 @@
+import { Repository } from "typeorm";
+import { Task } from "./task.entity";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class TaskRepository extends Repository<Task> {
+    // constructor(private )
+
+}
