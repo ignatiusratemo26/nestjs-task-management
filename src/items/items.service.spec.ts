89bee4c4a1ce7ec3228e13f94e56dc2ec0463570
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2025-09-29 16:14:40 +0300

    write findAll tet on unit tests

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -2,10 +2,11 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { ItemsService } from "./items.service"
 import { Item } from "./entities/item.entity";
 import { getRepositoryToken } from "@nestjs/typeorm";
-import { EntityManager } from "typeorm";
+import { EntityManager, Repository } from "typeorm";
 
 describe('ItemsService', () => {
     let service: ItemsService;
+    let itemsRepository: Repository<Item>;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -13,7 +14,9 @@ describe('ItemsService', () => {
                 ItemsService,
                 {
                     provide:  getRepositoryToken(Item),
-                    useValue: {},
+                    useValue: {
+                        find: jest.fn(),
+                    },
                 },
                 {
                     provide: EntityManager,
@@ -23,9 +26,15 @@ describe('ItemsService', () => {
         }).compile();
 
         service = module.get<ItemsService>(ItemsService);
+        itemsRepository= module.get<Repository<Item>>(getRepositoryToken(Item))
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
+
+    test('findAll', async ()=>{
+        await service.findAll();
+        expect(itemsRepository.find).toHaveBeenCalled();
+    })
 });
